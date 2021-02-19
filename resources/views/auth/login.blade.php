8358f09d03f274c879c607936329da6d22d9b877
Ivan Iman <you@example.com>
2021-02-19 15:29:47 +0300

    removing .idea-push:

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,6 @@
 <!-- BEGIN: Head -->
 <head>
     <meta charset="utf-8">
-    <!-- <link href="{{ asset('dist/images/logo.svg') }}" rel="shortcut icon"> -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="LEFT4CODE">
