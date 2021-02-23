282ac9346e516bc4cb105df4642ec223b6584032
Ivan Iman <you@example.com>
2021-02-23 12:00:22 +0300

    change using comand

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,13 +7,13 @@
 
         <title>{{ config('app.name', 'Kalonge') }}</title>
 
-        <link rel="stylesheet" href="/vendors/css/vendors.min.css">
+        <link rel="stylesheet" href="{{secure_asset('vendors/css/vendors.min.css')}}">
 
-        <link href="/dist/css/app.css" rel="stylesheet"/>
-        <link href="/css/app.css" rel="stylesheet" />
+        <link href="{{secure_asset('dist/css/app.css')}}" rel="stylesheet"/>
+        <link href=" {{secure_asset('css/app.css')}}" rel="stylesheet" />
         @routes
-        <script src="/js/app.js"></script>
-        <script src="/dist/js/app.js"></script>
+        <script src="{{secure_asset('js/app.js')}} "></script>
+        <script src="{{secure_asset('dist/js/app.js')}}"></script>
     </head>
     <body >
         @inertia
