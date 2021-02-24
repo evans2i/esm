544ad73b02bb9f30617045b5fe71e52b28ae177a
Ivan Iman <you@example.com>
2021-02-24 15:57:56 +0300

    seeder status some data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,19 +14,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(LaratrustSeeder::class);
-        $this->call(StudentStatusSeeder::class);
-        $this->call(AttendanceStatusSeeder::class);
-        $this->call(BookStatusSeeder::class);
-        $this->call(YearSeeder::class);
-        $this->call(MonthSeeder::class);
-        $this->call(DaySeeder::class);
-        $this->call(BedStatusSeeder::class);
+        $this->call(\StudentStatusSeeder::class);
+        $this->call(\AttendanceStatusSeeder::class);
+        $this->call(\BookStatusSeeder::class);
+        $this->call(\YearSeeder::class);
+        $this->call(\MonthSeeder::class);
+        $this->call(\DaySeeder::class);
+        $this->call(\BedStatusSeeder::class);
         // $this->call(AlertSettingSeeder::class);
         // $this->call(SmsTableSeeder::class);
-        $this->call(MeetingTableSeeder::class);
-        $this->call(PaymentMethodSeeder::class);
+//        $this->call(\MeetingTableSeeder::class);
+        $this->call(\PaymentMethodSeeder::class);
         // $this->call(PaymentTableSeeder::class);
-        $this->call(TimeZoneTableSeeder::class);
+        $this->call(\TimeZoneTableSeeder::class);
         // $this->call(AccountCategorySeeder::class);
         // $this->call(TransactionLedgerSeeder::class);
         // $this->call(CertificateTemplateSeeder::class);
