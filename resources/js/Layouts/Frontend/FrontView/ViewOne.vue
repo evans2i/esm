f8c5db0d0ab99dd7e3896f9f57c2db320fdd9f7f
Ivan Iman <you@example.com>
2021-04-29 19:00:31 +0300

    second view

--- resources/js/Layouts/Frontend/FrontView/ViewOne.vue
+++ resources/js/Layouts/Frontend/FrontView/ViewOne.vue
@@ -3,12 +3,9 @@
         <div class=" px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
             <!--Left Col-->
             <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left ">
-                <h1 v-if="van.institute" class="uppercase tracking-loose font-bold my-0 w-full text-white text-3xl">{{van.institute}}</h1>
-                <h1 v-else class="uppercase tracking-loose font-bold my-0 w-full text-white text-3xl">School One</h1>
+                <h1 class="uppercase tracking-loose font-bold my-0 w-full text-white text-3xl">School One</h1>
                 <h3  class="my-4 text-3xl font-medium  text-gray-400 leading-tight">
-                    <q v-if="van.salogan">{{van.salogan}}</q>
-                    <q v-else>Performance Depend On Good Education and Love From Parents and Teachers</q>
-
+                     <q>Performance Depend On Good Education and Love From Parents and Teachers</q>
                 </h3>
                 <div >
                     <p class="leading-normal text-white text-2xl mb-8">We Promise Your Child will be Better than Expected <img class="" class=""text-white src="/dist/images/iconmonstr-pencil-thin.svg" /></p>
